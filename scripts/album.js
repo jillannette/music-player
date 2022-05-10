526a3698af821be800213be623013c22c938dde4
Jill Arnold <jillarnold2913@gmail.com>
2022-05-10 17:29:46 -0400

    Final

--- scripts/album.js
+++ scripts/album.js
@@ -6,13 +6,38 @@ var createSongRow = function (songNumber, songName, songLength) {
    + '  <td class="song-item-duration">' + songLength + '</td>'
    + '</tr>'
    ;
+   
+  var $row = $(template);
 
-   var $row = $(template);
+  var handleSongClick = function() {
+      $(this).html(pauseButtonTemplate);
+    };
+  };
 
-   return $row;
+  var onHover = function () {
+    var songItem = $(this).find('.song-item-number');
+    
+    songItem.html(playButtonTemplate);
+  };
+  
+  var offHover = function () {
+    var songItem = $(this).find('.song-item-number');
+    var songNumber = songItem.attr('data-song-number');
+    
+    songItem.html(songNumber);
+  };
+  
+  $row.find('.song-item-number').click(handleSongClick);
+  $row.hover(onHover, offHover);
+  
+  return $row;
 };
+ 
+
 
 var setCurrentAlbum = function(album) {
+  currentAlbum = album;
+
   var $albumTitle = $('.album-view-title');
   var $albumArtist = $('.album-view-artist');
   var $albumReleaseInfo = $('.album-view-release-info');
@@ -31,3 +56,9 @@ var setCurrentAlbum = function(album) {
     $albumSongList.append($songRow);
   }
 };
+
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
+setCurrentAlbum(albums[0]);
+
